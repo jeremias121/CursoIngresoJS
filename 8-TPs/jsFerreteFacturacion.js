27686980cb1c59dcf60be4b5d050d1c7e35e7c96
jeremias121 <jeremias_6693@hotmail.com>
2020-02-07 12:21:24 -0300

    modificacion de ejercicio 1 del tp

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,36 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+var precio1
+var precio2
+var precio3
+var resultado
+
 function Sumar () 
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value);
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    resultado= precio1+precio2+precio3;
+
+        alert("El resultado es: " + resultado)
 }
 function Promedio () 
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value);
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    resultado= (precio1+precio2+precio3)/3;
+
+        alert("El promedio es: " + resultado)
 }
 function PrecioFinal () 
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value);
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    resultado= precio1+precio2+precio3+(precio1+precio2+precio3)*0.21;
+
+        alert("El Precio Final es: " + resultado)
 }

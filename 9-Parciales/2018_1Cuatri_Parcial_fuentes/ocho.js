2a78baae52f130d227f84c0275e9071381125944
alumno <alumno@utnfra.com.ar>
2020-02-26 22:15:30 -0300

    ejercicio 8 del examen

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+var letra;
+var numero;
 
+    do{
+    
+        numero=prompt("Ingrese un numero: ");
+        numero=parseInt(numero);
+        }while (isNaN(numero)|| numero<-100 || numero>100);
+    
+    }do{
+        letra=prompt("Ingrese una letra: ")
+    }while()
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var acumuladorNotas=0;
+    var promedio;
+    var notaBaja;
+    var sexoNotaBaja;
+    var varonesAprobados=0;
+    var contador =0
+    
+    while (contador<5) {
+        contador++;
+    
+        do{
+        nota=prompt("Ingrese la nota: ");
+        nota=parseInt(nota);
+        }while (isNaN(nota)|| nota<0 || nota>10);
+    
+        do{
+            sexo=prompt("Ingrese el genero: ");
+    
+        }while (sexo != "f" && sexo!= "m");
+    
+        acumuladorNotas+= nota;
+    
+        if(contador==1){
+   
+            notaBaja=nota;
+   
+        }else {
+   
+        if(nota<notaBaja){
+   
+            notaBaja= nota;
+   
+            sexoNotaBaja=sexo;
+   
+        }
+    }
+   
+    if (nota>=6 && sexo=="m")
+   
+     varonesAprobados++;
 
+    }
+
+    promedio= acumuladorNotas/contador;
+
+    alert("Promedio de las notas: "+ promedio);
+    alert("La nota mas baja es: "+ notaBaja+ " y es pertenece al sexo: "+ sexoNotaBaja);
+    alert("La cantidad de notas masculinas mas altas es: "+ varonesAprobados);
 }

17e025c20dcc79cc45b2f33abb19d35a3ef1d69c
alumno <alumno@utnfra.com.ar>
2020-02-27 22:03:32 -0300

    Ejercicios 8y9 del modelo de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,77 @@
 function mostrar()
 {
+var respuesta;
+var peso;
+var marca;
+var temperatura;
 
-}
+var temperaturaPares=0;
+var temperaturaNegativa=0;
+var primeraVez=true;
+var pesoMaximo;
+var pesoMinimo;
+var marcaMasPesada;
+var contadorPesos=0;
+var acumuladorPeso=0;
+
+
+    respuesta=confirm("Desea continuar?");
+    
+    while(respuesta){
+        
+        do{
+            marca=prompt("Ingrese una letra: ")
+        }while(!isNaN(marca));
+
+        do{
+        
+            peso=prompt("Ingrese un numero: ");
+            peso=parseInt(peso);
+        }while (isNaN(peso)|| peso<1 || peso>100);
+        
+        do{
+            temperatura=prompt("Ingrese un numero: ");
+            temperatura=parseInt(temperatura);
+        }while (isNaN(temperatura)|| temperatura<-30 || temperatura>30);
+        
+        if(temperatura%2==0){
+            temperaturaPares++;        
+        }
+
+        if(temperatura<0){
+            temperaturaNegativa++;;
+        }
+
+        if(primeraVez){
+            primeraVez= false;
+            pesoMinimo=peso;
+            pesoMaximo=peso;
+            marcaMasPesada=marca;
+
+        }else{
+            if(peso>pesoMaximo){
+                pesoMaximo= peso;
+                marcaMasPesada=marca;
+            }
+            if(peso<pesoMinimo){
+                pesoMinimo=peso;
+            }
+        }
+        
+        acumuladorPeso+=peso;
+        contadorPesos++;
+
+        respuesta=confirm("Desea Continuar?")
+    }
+
+    document.write("Temperaturas Pares:"+ temperaturaPares+" <br>");
+    document.write("La Marca con el producto mas pesada es: "+marcaMasPesada+"<br>");
+        if(contadorPesos>0){
+        
+            document.write("Promedio de todos los pesos de los productos: "+ acumuladorPeso/contadorPesos+"<br>");
+        }
+    document.write("Los Cantidad de productos que se concervan a menos de 0º son: "+ temperaturaNegativa+"<br>");    
+    document.write("El Peso Maximo es: "+pesoMaximo+" y el Peso Minimo es: "+pesoMinimo+"<br>");
+    
+      
+} 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -2,14 +2,86 @@ function mostrar()
 {
 var letra;
 var numero;
+var respuesta;
+var contadorPares=0;
+var contadorImpares=0;
+var contadorCeros=0;
+var contadorPositivos=0;
+var acumuladorPositivo=0;
+var acumuladorNegativo=0;
+var primeraVez= true;
+var min;
+var max;
+var maxLetra;
+var minLetra;
 
+respuesta=confirm("Desea continuar?");
+
+while(respuesta){
     do{
     
         numero=prompt("Ingrese un numero: ");
         numero=parseInt(numero);
-        }while (isNaN(numero)|| numero<-100 || numero>100);
+    }while (isNaN(numero)|| numero<-100 || numero>100);
     
-    }do{
+    do{
         letra=prompt("Ingrese una letra: ")
-    }while()
+    }while(!isNaN(letra));
+
+
+    if(numero%2==0){
+        contadorPares++;        
+        
+    }else if(numero==0){
+        
+        contadorCeros++;
+    
+    }else{
+
+        contadorImpares++;
+    }
+    if (numero>0){
+    
+        acumuladorPositivo+=numero;
+        contadorPositivos++;
+
+    }else if(numero<0){
+    
+        acumuladorNegativo+=numero;
+    
+    } 
+
+    if(primeraVez){
+        primeraVez= false;
+        min=numero;
+        max=numero;
+        minLetra= letra;
+        maxLetra=letra;
+
+    }else{
+        if(numero>max){
+            max= numero;
+            maxLetra= letra;
+        }
+        if(numero<min){
+            min=numero;
+            minLetra=letra;
+        }
+    }
+
+    respuesta=confirm("Desea Continuar?");
+
+}
+
+document.write("Cantidad de Numeros Pares:"+ contadorPares+" <br>");
+document.write("Cantidad de Numeros Impares: "+contadorImpares+"<br>");
+document.write("Cantidad de Ceros: "+contadorCeros+"<br>");
+    if(contadorPositivos>0){
+    
+        document.write("Promedio de Numeros Positivos: "+ acumuladorPositivo/contadorPositivos+"<br>");
+    }
+document.write("La suma de los Numeros Negativos es: "+ acumuladorNegativo+"<br>");    
+document.write("El Numero Maximo es: "+max+" y su letra es: "+maxLetra+"<br>");
+document.write("El Numero Minimo es: "+min+" y su letra es: "+minLetra);
+  
 }

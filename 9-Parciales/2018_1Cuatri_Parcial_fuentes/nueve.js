619f82a2c44f342424e0b8d7a765a8987fa4d189
jeremias121 <jeremias_6693@hotmail.com>
2020-03-05 01:19:00 -0300

    modificacion de ejercicio de los modelos de examen n9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -20,17 +20,17 @@ var acumuladorPeso=0;
     while(respuesta){
         
         do{
-            marca=prompt("Ingrese una letra: ")
+            marca=prompt("Ingrese la marca: ")
         }while(!isNaN(marca));
 
         do{
         
-            peso=prompt("Ingrese un numero: ");
+            peso=prompt("Ingrese el peso: ");
             peso=parseInt(peso);
         }while (isNaN(peso)|| peso<1 || peso>100);
         
         do{
-            temperatura=prompt("Ingrese un numero: ");
+            temperatura=prompt("Ingrese la temperatura: ");
             temperatura=parseInt(temperatura);
         }while (isNaN(temperatura)|| temperatura<-30 || temperatura>30);
         

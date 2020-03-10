68a81c69ed6affe6b45930998733088fd15d5434
jeremias121 <jeremias_6693@hotmail.com>
2020-03-09 23:27:43 -0300

    modificacion de ejercicio de los modelos de examen

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,63 +1,88 @@
-
 function mostrar()
 {
-var contador=0;
-var material;
-var precioMaterial;
-var unidades;
-var marca;
-var fabricante;
-var barbijoMasCaro;
-var fabricanteMasCaro;
-var unidadesMasCaras;
 
-
-while (contador<5) {
-    contador++;
-
-    do{
+    var contador=0;
+    var material;
+    var precioMaterial;
+    var unidades;
+    var marca;
+    var fabricante;
+    var barbijoMasCaro;
+    var fabricanteMasCaro;
+    var unidadMasCaras;
+    var unidadMayorDeJabon=0;
+    var cantidadMayorDeUnidades;
+    var fabricanteConMayorUnidades;
+    var materialConMayorUnidades;
     
-        material=prompt("Ingrese el material si es barbijo, jabón o alcohol");
     
-    }while (!isNaN(material)|| material!="barbijo"&& material!="jabón"&& material!="alcohol") {
-        
-    }do{
-
-        precioMaterial=parseInt(prompt("Ingrece el precio del material"));
-
-    }while (isNaN(precioMaterial)|| precioMaterial<100|| precioMaterial>300) {
-
-    }do{
-
-        unidades=parseInt(prompt("Ingrece la cantidad de unidades que pueden ser de 1 a 1000"));
+    while (contador<5) {
+        contador++;
     
-    }while (isNaN(unidades)|| unidades<1||unidades>1000) {
+        do{
         
-    }do{
-
-        marca=prompt("Ingrese la marca del material");
-   
-    }while (!isNaN(marca)) {
+            material=prompt("Ingrese el material si es barbijo, jabón o alcohol");
         
-    }do{
-
-        fabricante= prompt("Ingrese el fabricante del material");
-
-    }while (!isNaN(fabricante)) {
+        }while (!isNaN(material)|| material!="barbijo"&& material!="jabón"&& material!="alcohol") {
+            
+        }do{
+    
+            precioMaterial=parseInt(prompt("Ingrece el precio del material"));
+    
+        }while (isNaN(precioMaterial)|| precioMaterial<100|| precioMaterial>300) {
+    
+        }do{
+    
+            unidades=parseInt(prompt("Ingrece la cantidad de unidades que pueden ser de 1 a 1000"));
         
-    }
-    if(contador==1&& material=="barbijo"){
-        barbijoMasCaro=precioMaterial;
-        fabricanteMasCaro=fabricante;
-        unidadesMasCaras=unidades;
-    }else{
-        if(precioMaterial){
-
+        }while (isNaN(unidades)|| unidades<1||unidades>1000) {
+            
+        }do{
+    
+            marca=prompt("Ingrese la marca del material");
+       
+        }while (!isNaN(marca)) {
+            
+        }do{
+    
+            fabricante= prompt("Ingrese el fabricante del material");
+    
+        }while (!isNaN(fabricante)) {
+            
+        }
+        if(contador==1&& material=="barbijo"){
+            barbijoMasCaro=precioMaterial;
+            fabricanteMasCaro=fabricante;
+            unidadMasCaras=unidades;
+    
+        }else{
+            if(precioMaterial>barbijoMasCaro){
+                
+                barbijoMasCaro=precioMaterial;
+                fabricanteMasCaro=fabricante;
+                unidadMasCaras=unidades;
+            }
+        }
+        if (material=="jabón") {
+            
+            unidadMayorDeJabon+=unidades;
+    
+        }
+        if (contador ==1 ) {
+            cantidadMayorDeUnidades=unidades;
+            fabricanteConMayorUnidades=fabricante;
+            materialConMayorUnidades=material;
+        }else{
+            if(cantidadMayorDeUnidades<unidades){
+                cantidadMayorDeUnidades=unidades;
+                fabricanteConMayorUnidades=fabricante;
+                materialConMayorUnidades=material;
+            }
         }
     }
+    alert("El precio del barbijo mas caro es: "+ barbijoMasCaro+" ,la cantidad de unidades son: "+ unidadMasCaras+" y su fabricante es: "+ fabricanteMasCaro);
+    alert("El material con mas unidades es: "+materialConMayorUnidades+" y su fabricante es: "+fabricanteConMayorUnidades+" , y la cantidad de unidades es: "+cantidadMayorDeUnidades);
+    alert("la cantidad total de unidades de jabón son: "+ unidadMayorDeJabon);
+}
     
-
-
-}
-
-}
+    

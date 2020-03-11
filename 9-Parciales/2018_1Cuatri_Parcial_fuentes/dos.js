7bb008b00c5abca04a8b29080a59cc5b78d175d0
jeremias121 <jeremias_6693@hotmail.com>
2020-03-11 00:55:02 -0300

    modificacion de ejercicio de los modelos de examen

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,88 @@
 function mostrar()
 {
-  var nombre;
-  var localidad;
+var pregunta;
+var nombre;
+var edad;
+var sexo;
+var estadoCivil;
+var primeravezHombre=true;
+var primeravezMujer=true;
+var nombreCasadoMasJoven;
+var edadCasadoMasJoven;
+var cantidadHombresSolteros=0;
+var cantidadMujeres=0;
+var acumuladorEdadMujeres=0;
+var edadMasGrandeMujer;
+var nombreMasGradeMujer;
 
-  nombre= document.getElementById("elNombre").value;
-  localidad= document.getElementById("laLocalidad").value;
+pregunta=confirm("¿Deseas Comenzar?");
 
-  alert("Usted es: "+ nombre + ", y vive en la localidad de: "+ localidad);
+while (pregunta) {
+  
+
+  do{
+  
+      nombre=prompt("Ingrese el mombre:");
+  
+  }while (!isNaN(nombre)) {
+      
+  }do{
+
+      edad=parseInt(prompt("Ingrece la edad:"));
+
+  }while (isNaN(edad)) {
+
+  }do{
+
+      sexo=(prompt("Ingrece el sexo si es m o f: "));
+  
+  }while (!isNaN(sexo)|| sexo!="m" &&  sexo!="f") {
+      
+  }do{
+
+      estadoCivil= prompt("Ingrese el estado civil: ");
+
+  }while (!isNaN(estadoCivil) || estadoCivil!="soltero" && estadoCivil!="casado" && estadoCivil!="novio") {
+    
+  }
+  if (sexo=="m"&& primeravezHombre && estadoCivil=="casado"){
+      
+    primeravezHombre=false;    
+    nombreCasadoMasJoven= nombre;
+    edadCasadoMasJoven=edad;
+  }else{
+    if(sexo=="m"&&edad<edadCasadoMasJoven && estadoCivil=="casado"){
+      nombreCasadoMasJoven=nombre;
+      edadCasadoMasJoven=edad;
+    }
+  }
+  if(sexo=="f"){
+
+    cantidadMujeres++;
+    acumuladorEdadMujeres+=edad;
+  }
+
+  if (sexo=="m"&& estadoCivil=="soltero") {
+    
+    cantidadHombresSolteros++;
+  }
+  if(sexo="f" && primeravezMujer){
+
+    primeravezMujer=false;
+    nombreMasGradeMujer=nombre;
+    edadMasGrandeMujer=edad;
+  }else{
+    if(sexo="f"&& edad>edadMasGrandeMujer){
+      nombreMasGradeMujer=nombre;
+      edadMasGrandeMujer=edad;
+    }
+  }
+pregunta=confirm("¿Deseas continuar?");  
+}
+
+document.write("El hombre casado mas joven es: "+ nombreCasadoMasJoven+" y tiene: "+edadCasadoMasJoven+"<br>");
+document.write("el promedio de la edad de las mujeres es: "+acumuladorEdadMujeres/cantidadMujeres+"<br>");
+document.write("La cantidad de hombres solteros es: "+ cantidadHombresSolteros+"<br>");
+document.write("La Mujer de mayor edad es: "+ nombreMasGradeMujer+" y su edad es: "+edadMasGrandeMujer+"<br>");
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -10,7 +10,7 @@ preciototal=precio- ((precio*porcentaje)/100);
 
 document.getElementById("elPrecioFinal").value= preciototal;
 
-}
+}//punto
 var ancho;
 var largo;
 var perimetro;
@@ -20,3 +20,11 @@ largo=parseInt(prompt("ingrese el largo del rectangulo"));
 perimetro=2*ancho+2*largo;
 
     alert("El perimetro del rectangulo es: "+ perimetro);
+//punto dos
+    var nombre;
+    var localidad;
+  
+    nombre= document.getElementById("elNombre").value;
+    localidad= document.getElementById("laLocalidad").value;
+  
+    alert("Usted es: "+ nombre + ", y vive en la localidad de: "+ localidad);
